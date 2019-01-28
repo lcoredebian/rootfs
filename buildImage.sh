88b22166360efa917b5536777d12121762e021e0
Luca <luc.fabbian@gmail.com>
2019-01-28 22:14:22 +0100

    Added linux tools packages

--- buildImage.sh
+++ buildImage.sh
@@ -17,7 +17,7 @@ rm -r dist/debian_$ARCH.tar.xz
 
 # Download packages with deboostrap and install them
 # If your architecture is not compatible with the one you're building, qemu will be automatically called
-qemu-debootstrap --variant=minbase --include=x11vnc,xterm,xvfb,bspwm --arch=$ARCH stable ./image $MIRROR
+qemu-debootstrap --variant=minbase --include=x11vnc,xterm,xvfb,bspwm,inotify-tools,psmisc,xdotool --arch=$ARCH stable ./image $MIRROR
 
 # Create compressed .tar.xz inside dist/ folder
 # Dist format will have the best compression rate possible (which is -9)

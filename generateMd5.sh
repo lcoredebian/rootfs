c33643b29d7dd43554fd5fd608e3308018007be2
Luca <luc.fabbian@gmail.com>
2019-05-19 12:55:25 +0200

    Reformat md5 output

--- generateMd5.sh
+++ generateMd5.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-md5sum ./dist/debian_i386.tar.xz > ./dist/md5.txt
-md5sum ./dist/debian_amd64.tar.xz >> ./dist/md5.txt
-md5sum ./dist/debian_armhf.tar.xz >> ./dist/md5.txt
-md5sum ./dist/debian_arm64.tar.xz >> ./dist/md5.txt
+# Generate md5 check codes for every rootfs file.
+# Output format is the one of kotlin hashmaps.
+echo "\"i386\"   to \"$(md5sum ./dist/debian_i386.tar.xz   |cut -f 1 -d " ")\"," >  ./dist/md5.txt
+echo "\"amd64\"  to \"$(md5sum ./dist/debian_amd64.tar.xz  |cut -f 1 -d " ")\"," >> ./dist/md5.txt
+echo "\"armhf\"  to \"$(md5sum ./dist/debian_armhf.tar.xz  |cut -f 1 -d " ")\"," >> ./dist/md5.txt
+echo "\"arm64\"  to \"$(md5sum ./dist/debian_arm64.tar.xz  |cut -f 1 -d " ")\""  >> ./dist/md5.txt
